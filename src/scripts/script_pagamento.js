715421313e6b874c1dcf68525f66b2d5cbf0020d
Marcoslsilva <112900211+Marcoslsilva@users.noreply.github.com>
2025-02-14 20:00:37 +0000

    ultimas att

--- src/scripts/script_pagamento.js
+++ src/scripts/script_pagamento.js
@@ -5,6 +5,11 @@ const op_pagamento = document.getElementById("forma_pagamento");
 const pagamento = document.getElementById("pagamento");
 const fechar = document.getElementById("fechar")
 const imprimir = document.getElementById("imprimir")
+const cor = document.getElementById("cor")
+const modelo = document.getElementById("modelo")
+const tamanho = document.getElementById("tamanho")
+const placa = document.getElementById("placa")
+const preco = document.getElementById("preco")
 
 overlay.style.display = "none";
 console.log("Overlay encontrado:", overlay);
@@ -13,13 +18,13 @@ imprimir.onclick = imprimirTicket;
 fechar.onclick = fecharOverlay;
 ticket.onclick = function(event) {
     event.preventDefault();
-    console.log("Abeindo o overlay");
-
+    
     overlay.style.display = "block";
   
       
 
 pagamento.innerText = op_pagamento.value;
+cor_veiculo.innerText = cor;
 }
 
 
@@ -30,3 +35,4 @@ function fecharOverlay() {
 function imprimirTicket() {
     window.print()
 }
+
