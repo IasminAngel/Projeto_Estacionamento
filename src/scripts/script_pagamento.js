67d640b9fe8bd482c9f978539ce35650b1665b39
Marcoslsilva <112900211+Marcoslsilva@users.noreply.github.com>
2025-02-14 19:20:10 +0000

    mais att

--- src/scripts/script_pagamento.js
+++ src/scripts/script_pagamento.js
@@ -1,11 +1,32 @@
-const ticket = document.getElementById("confirmar")
-const overlay = document.getElementById("overlay")
-const conteudo = document.getElementById("conteudo")
+const ticket = document.getElementById("Confirmar");
+const overlay = document.getElementById("overlay");
+const conteudo = document.getElementById("conteudo");
+const op_pagamento = document.getElementById("forma_pagamento");
+const pagamento = document.getElementById("pagamento");
+const fechar = document.getElementById("fechar")
+const imprimir = document.getElementById("imprimir")
 
-ticket.onclick(abrirTicket())
+overlay.style.display = "none";
+console.log("Overlay encontrado:", overlay);
 
-function abrirTicket(){
-    overlay.style.display = "block"  
-    conteudo.style.display = "block"
-    
+imprimir.onclick = imprimirTicket;
+fechar.onclick = fecharOverlay;
+ticket.onclick = function(event) {
+    event.preventDefault();
+    console.log("Abeindo o overlay");
+
+    overlay.style.display = "block";
+  
+      
+
+pagamento.innerText = op_pagamento.value;
+}
+
+
+function fecharOverlay() {
+    overlay.style.display = "none"
+}
+
+function imprimirTicket() {
+    window.print()
 }
