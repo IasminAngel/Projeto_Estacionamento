25fe4cbd4702be421612774370c69d7df5b4e99d
João Miranda <143547685+joaomiranda14@users.noreply.github.com>
2025-02-14 13:57:31 +0000

    Conexão aplicada

--- src/database/db.js
+++ src/database/db.js
@@ -1,1 +1,30 @@
+const mysql = require('mysql2'); 
+const express = require('express'); 
 
+const app = express();
+
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'wallpark'
+});
+
+conexao.connect(function(erro) {
+    if (erro) {
+        console.error('Erro ao conectar ao banco:', erro);
+        return;
+    }
+    console.log('Conexão efetuada com sucesso!');
+});
+
+// Rota de teste
+app.get('/', function(req, res) {
+    res.send('Utilizando o nodemon');
+});
+
+
+app.listen(3306, () => {
+    console.log('Servidor rodando na porta 8080');
+});
